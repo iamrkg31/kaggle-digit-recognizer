4234ab2f41bcb1f78a5e74ac152ba5546b5b68ed
iamrkg31 <rahul199431@gmail.com>
2018-10-29 12:15:47 +0530

    Update

--- architectures/cnn.py
+++ architectures/cnn.py
@@ -1,44 +1,48 @@
 import tensorflow as tf
 
 class CNN(object):
-    def __init__(self, filter_sizes, num_filters, input_X_shape, num_classes=10, learning_rate=0.001):
+    def __init__(self, filter_sizes, num_filters, input_X_shape, num_classes=10, learning_rate=0.001, channels=1):
         # Placeholders
         self.input_x = tf.placeholder(tf.float32, [None, input_X_shape[0], input_X_shape[1]], name="input_X")
         self.input_x_expanded = tf.expand_dims(self.input_x, -1)
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_Y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
-        # Create a convolution + maxpool layer for each filter size
-        pooled_outputs = []
+        # Create a convolution and maxpool layer for each filter size
+        pooled_output = self.input_x_expanded
+        in_channels = channels
+        out_channels = num_filters
         for i, filter_size in enumerate(filter_sizes):
             # Convolution Layer
-            filter_shape = [filter_size, input_X_shape[1], 1, num_filters]
+            filter_shape = [filter_size, filter_size, in_channels, out_channels]
             W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1))
-            b = tf.Variable(tf.constant(0.1, shape=[num_filters]))
+            b = tf.Variable(tf.constant(0.1, shape=[out_channels]))
             conv = tf.nn.conv2d(
-                self.input_x_expanded,
+                pooled_output,
                 W,
                 strides=[1, 1, 1, 1],
-                padding="VALID",
+                padding="SAME",
                 name="conv")
             # Apply nonlinearity
             h = tf.nn.relu(tf.nn.bias_add(conv, b))
             # Maxpooling over the outputs
             pooled = tf.nn.max_pool(
                 h,
-                ksize=[1, input_X_shape[0] - filter_size + 1, 1, 1],
-                strides=[1, 1, 1, 1],
-                padding='VALID',
+                ksize=[1, filter_size, filter_size, 1],
+                strides=[1, filter_size, filter_size, 1],
+                padding='SAME',
                 name="pool")
-            pooled_outputs.append(pooled)
-        num_filters_total = num_filters * len(filter_sizes)
-        h_pool = tf.concat(pooled_outputs,3)
-        h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total])
+            pooled_output = pooled
+            in_channels = out_channels
+            out_channels = 2 * out_channels
+
+        # Flatten the pooled result
+        h_pool_flat = tf.reshape(pooled_output, [-1, int(out_channels/2)])
         h_tanh = tf.nn.tanh(h_pool_flat)
         h_drop = tf.nn.dropout(h_pool_flat, self.dropout_keep_prob)
 
         # Create weight and bias variable
-        self.weight = tf.Variable(tf.truncated_normal([num_filters_total, num_classes], stddev=0.1))
+        self.weight = tf.Variable(tf.truncated_normal([int(out_channels/2), num_classes], stddev=0.1))
         self.bias = tf.Variable(tf.constant(0.1, shape=[num_classes]))
 
         # Create logits
